2eb29341e401d4ab48b63e8e8bc64239d87628f2
TC <37568161+momongss@users.noreply.github.com>
2021-02-02 15:44:57 +0900

    creating new Memo prototype

--- src/components/Memo.js
+++ src/components/Memo.js
@@ -6,36 +6,34 @@ export default class Memo {
     const $element = document.createElement("textarea");
     $element.className = "new-note-content";
     $element.style.position = "absolute";
-    $element.style.width = `${this.noteWidth}px`;
-    $element.style.height = `${this.noteHeight}px`;
+    $element.style.width = `${MEMOWIDTH}px`;
+    $element.style.height = `${MEMOHEIGHT}px`;
     $element.style.zIndex = "2147483647";
     $element.style.fontSize = "14px";
 
     $element.focus();
 
-    window.addEventListener("mousemove", this.trackingMouse);
+    window.addEventListener("mousemove", trackingMouse);
     $element.addEventListener("click", () => {
-      this.$element = $element;
-      window.removeEventListener("mousemove", this.trackingMouse);
-      console.log("new Memo generated");
+      window.removeEventListener("mousemove", trackingMouse);
+
+      console.log("new Memo created");
     });
 
     document.body.appendChild($element);
 
-    console.log("creating new Memo");
-  }
+    function trackingMouse(e) {
+      let posX = e.pageX - 10;
+      let posY = e.pageY - 10;
+      if (posX < 0) posX = 0;
+      else if (posX + MEMOWIDTH > document.body.offsetWidth)
+        posX = document.body.offsetWidth - MEMOWIDTH;
+      if (posY < 0) posY = 0;
+      else if (posY + MEMOHEIGHT > document.body.scrollHeight)
+        posY = document.body.scrollHeight - MEMOHEIGHT;
 
-  trackingMouse(e) {
-    let posX = e.pageX - 10;
-    let posY = e.pageY - 10;
-    if (posX < 0) posX = 0;
-    else if (posX + MEMOWIDTH > document.body.offsetWidth)
-      posX = document.body.offsetWidth - MEMOWIDTH;
-    if (posY < 0) posY = 0;
-    else if (posY + MEMOHEIGHT > document.body.scrollHeight)
-      posY = document.body.scrollHeight - MEMOHEIGHT;
-
-    this.$element.style.left = `${posX}px`;
-    this.$element.style.top = `${posY}px`;
+      $element.style.left = `${posX}px`;
+      $element.style.top = `${posY}px`;
+    }
   }
 }
